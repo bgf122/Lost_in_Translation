86cd6f1eb2255d82a54448475d93d0a5232d7e89
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-01-27 22:36:14 +0200

    feat(router): install router-dom and add base of routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import Login from './components/Login/Login';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <Login>
-      
-    </Login>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/translate" element={<Translate />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+
+        
+      </div>
+    </BrowserRouter>
   );
 }
 
