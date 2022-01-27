72de764379fd92b073727c311135e8997b80111f
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-01-27 23:27:26 +0200

    fix(router): fix merging errors and finish basic routing with views

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login/Login";
+import Start from "./views/Start";
+import Translate from "./views/Translate";
+import Profile from "./views/Profile";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/translate" element={<Translate />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-
-        
+        <Container className="p-3">
+          <Routes>
+            <Route path="/" element={<Start />} />
+            <Route path="/translate" element={<Translate />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </Container>
       </div>
     </BrowserRouter>
   );
