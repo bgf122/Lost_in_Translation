b05deb96ec0283c6b1d24cbf348fa7fe27745493
Sasu Korhonen <smage90@hotmail.com>
2022-01-31 10:44:56 +0200

    feat: add new translations to API

---
+++ src/api/translation.js
@@ -0,0 +1,20 @@
+import { createHeaders, apiUrl } from ".";
+
+export const addTranslation = async (user, translation) => {
+    try {
+        const response = await fetch(`${apiUrl}/${user.id}`, {
+            method: "PATCH",
+            headers: createHeaders(),
+            body: JSON.stringify({
+              translations: [...user.translations, translation],
+            }),
+          });
+        if (!response.ok) {
+            throw new Error('Could not update translations history')
+        }
+        const data = await response.json();
+        return [null, data];
+      } catch (error) {
+        return [error.message, []];
+      }
+}   
